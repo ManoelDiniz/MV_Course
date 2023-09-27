6915c737c9ff4b666c6c6f4d3379a0f7fbccd7a3
ManoelDiniz <dinizpiimentel@gmail.com>
2023-09-27 19:47:16 -0300

    clean project

---
+++ src/app/pages/register/UserRegister.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import { InputRegister } from "./components/InputUserRegister"
+import './components/UserRegister.css'
+import mvCourseImage from './components/Mv_course.png'
+
+export const Register = () => {
+  const [ Login, setLogin ] = useState('')
+  const [ Name_user, setName_user ] = useState('')
+  const [ Senha, setPass ] = useState('')
+  const [ CPF, setCPf ] = useState('')
+  const [ Email, setEmail ] = useState('')
+  return(
+    <body>
+      <div className="input-register">
+        <img src={mvCourseImage} alt="MV Course" />
+        
+        <h1>Tela de Cadastro</h1>
+        <div className="Input-register2">
+          < InputRegister 
+            placehoder ='Login   '
+            value= {Login}
+            onChange={(newValue) =>setLogin(newValue)}
+            className='InputRegister'
+            />
+            < InputRegister 
+            placehoder ='Nome do usuario   '
+            value= {Name_user}
+            onChange={(newValue) =>setName_user(newValue)}
+            className='InputRegister'
+            />
+            
+            < InputRegister 
+            placehoder ='Senha  '
+            value= {Senha}
+            type="password"
+            onChange={(newValue) =>setPass(newValue)}
+            className='InputRegister'
+            />
+            
+            < InputRegister 
+            placehoder ='CPF  '
+            value= {CPF}
+            type="number"
+            onChange={(newValue) =>setCPf(newValue)}
+            className='InputRegister'
+            />
+            
+            < InputRegister 
+            placehoder ='Email  '
+            value= {Email}
+            onChange={(newValue) =>setEmail(newValue)}
+            className='InputRegister'
+            />
+        </div>
+        
+        <h3> Por favor Preencha todos os campos </h3>
+        <button>Registrar</button>
+
+      </div>
+    </body>
+  )
+}
--- src/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { firstController } from "./controller/FirstController";
-
-const router: Router = Router();
-///Routers
-router.get('/', firstController.home)
-
-export { router }
